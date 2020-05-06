17902edd2634fd74a623bab8c2d820db92c3322f
Elena Grassi <grassi.e@gmail.com>
2020-05-06 13:42:34 +0200

    Biodiversa upto4 and lost striplots fix

--- local/src/striplots.R
+++ local/src/striplots.R
@@ -6,6 +6,8 @@ args <- commandArgs(trailingOnly=TRUE)
 input <- args[[1]]
 ddsf <- args[[2]]
 outputplotdir <- args[[3]]
+what <- args[[4]]
+
 
 load(ddsf)
 
@@ -13,11 +15,10 @@ striplots <- function(gene, dds) {
     if (!any(rownames(dds) == gene)) {
         return()
      }
-      data<-plotCounts(dds, gene, intgroup="chemio", returnData=T)
-  data$chemio <- factor(data$chemio, levels=c("nonresp", "grey", "resp"))
+      data<-plotCounts(dds, gene, intgroup=what, returnData=T)
     data <- data[order(data$count),]
-    e2 <- ggplot(data, aes(x = chemio, y = count))
-      e3 <- e2 + geom_jitter(  aes(shape = chemio, color = chemio),   position = position_jitter(0.2),size = 3) + stat_summary( aes(color = chemio), fun.data="mean_sdl",  fun.args = list(mult=1),  geom = "pointrange",  size = 0.4, color="darkgreen")+     scale_color_manual(values =  c("#FC4E07", "#778899","#00AFBB"))+theme_bw()+scale_y_continuous(trans='log10')+labs(color = "Xeno", x="Xeno", shape="Xeno", y="Log10(nreads)")
+    e2 <- ggplot(data, aes_string(x = what, y = "count"))
+      e3 <- e2 + geom_jitter(  aes_string(shape = what, color = what),   position = position_jitter(0.2),size = 3) + stat_summary( aes_string(color = what), fun.data="mean_sdl",  fun.args = list(mult=1),  geom = "pointrange",  size = 0.4, color="darkgreen")+theme_bw()+scale_y_continuous(trans='log10')+labs(color = "Xeno", x="Xeno", shape="Xeno", y="Log10(nreads)")
       ggsave(paste0(gene, ".png"))
 }
 
