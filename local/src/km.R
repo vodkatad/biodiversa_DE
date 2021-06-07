5625db1ee856ccd49928c56ba921fbf4cacdae53
Elena Grassi <grassi.e@gmail.com>
2021-06-07 10:23:36 +0200

    km vs in vivo responses

--- local/src/km.R
+++ local/src/km.R
@@ -5,18 +5,19 @@ ctx <- read.table('/mnt/trcanmed/snaketree/prj/pdxopedia/local/share/data/treats
 iri <- read.table('/mnt/trcanmed/snaketree/prj/pdxopedia/local/share/data/treats/may2020/irinotecan_w3.txt', sep="\t", header=T, row.names = NULL)
 colnames(ctx) <- c('model', 'ctx')
 colnames(iri) <- c('model', 'irino')
-km <- read.table('/mnt/trcanmed/snaketree/prj/pdxopedia/local/share/data/turin_n613_xeno_qn_genefilter_opt2_KMcalls_13052021.txt', sep="\t", header=T)
-head(km)
+km <- read.table('/mnt/trcanmed/snaketree/prj/pdxopedia/local/share/data/turin_n613_xeno_qn_genefilter_opt2_KMcalls_13052021.txt', sep="\t", header=T, stringsAsFactors = FALSE)
 
-meta <- read.table('/mnt/trcanmed/snaketree/prj/RNASeq_biod_metadata/dataset/july2020_starOK/selected_metadata_annot_final_nolinfo_nooutlier_ctx', sep="\t", header=T)
+km <- km[km$KM.Subtypes != "Mixed",]
+km$model <- substr(km$Sample_ID, 0, 7)
+km <- km[grepl("LMX", km$Sample_ID),]
 
+#meta <- read.table('/mnt/trcanmed/snaketree/prj/RNASeq_biod_metadata/dataset/july2020_starOK/selected_metadata_annot_final_nolinfo_nooutlier_ctx', sep="\t", header=T)
+#check all basali= yes!
 ### irino
 mi <- merge(km, iri, by="model", all.x=T)
 mi$class <- ifelse(is.na(mi$irino), 'Undetermined', ifelse(mi$irino < 35, 'Responder', "Non-Responder"))
-mix <- mi[grepl("LMX", mi$Sample_ID),]
 
-
-pld <- mix %>% 
+pld <- mi %>% 
   group_by(KM.Subtypes,class) %>% 
   summarise(count = n()) %>% 
   mutate(perc = count/sum(count))
@@ -25,26 +26,24 @@ ggplot(pld, aes(x = factor(KM.Subtypes), y = perc*100, fill = factor(class))) +
   geom_bar(stat="identity", width = 0.7) +
   labs(x = "KM", y = "percent", fill = "Irinotecan 3Wd%") +
   theme_bw(base_size = 14)+scale_fill_manual(values=c('red','darkblue','grey'))
-
-pld <- mix %>% 
-  group_by(KM.prediction,class) %>% 
-  summarise(count = n()) %>% 
-  mutate(perc = count/sum(count))
-
-ggplot(pld, aes(x = factor(KM.prediction), y = perc*100, fill = factor(class))) +
-  geom_bar(stat="identity", width = 0.7) +
-  labs(x = "KMpred", y = "percent", fill = "Irinotecan 3Wd%") +
-  theme_bw(base_size = 14)+scale_fill_manual(values=c('red','darkblue','grey'))
+# 
+# pld <- mix %>% 
+#   group_by(KM.prediction,class) %>% 
+#   summarise(count = n()) %>% 
+#   mutate(perc = count/sum(count))
+# 
+# ggplot(pld, aes(x = factor(KM.prediction), y = perc*100, fill = factor(class))) +
+#   geom_bar(stat="identity", width = 0.7) +
+#   labs(x = "KMpred", y = "percent", fill = "Irinotecan 3Wd%") +
+#   theme_bw(base_size = 14)+scale_fill_manual(values=c('red','darkblue','grey'))
 
 table(mix$KM.Subtypes)
 table(mix$KM.prediction)
 ### cetuxi
 mi <- merge(km, ctx, by="model", all.x=T)
 mi$class <- ifelse(is.na(mi$ctx), 'Undetermined', ifelse(mi$ctx < 35, 'Responder', "Non-Responder"))
-mix <- mi[grepl("LMX", mi$Sample_ID),]
 
-
-pld <- mix %>% 
+pld <- mi %>% 
   group_by(KM.Subtypes,class) %>% 
   summarise(count = n()) %>% 
   mutate(perc = count/sum(count))
@@ -54,15 +53,6 @@ ggplot(pld, aes(x = factor(KM.Subtypes), y = perc*100, fill = factor(class))) +
   labs(x = "KM", y = "percent", fill = "Cetuxi 3Wd%") +
   theme_bw(base_size = 14)+scale_fill_manual(values=c('red','darkblue','grey'))
 
-pld <- mix %>% 
-  group_by(KM.prediction,class) %>% 
-  summarise(count = n()) %>% 
-  mutate(perc = count/sum(count))
-
-ggplot(pld, aes(x = factor(KM.prediction), y = perc*100, fill = factor(class))) +
-  geom_bar(stat="identity", width = 0.7) +
-  labs(x = "KMpred", y = "percent", fill = "Cetuxi 3Wd%") +
-  theme_bw(base_size = 14)+scale_fill_manual(values=c('red','darkblue','grey'))
 
 table(mix$KM.Subtypes)
 table(mix$KM.prediction)
