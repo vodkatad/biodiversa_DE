635c20d4c24bdd388128b59ebd30672be33c0859
Elena Grassi <grassi.e@gmail.com>
2021-11-29 17:38:28 +0000

    Charles River conf and V3 (split base/deg)

---
+++ local/src/waterfall_v2.R
@@ -0,0 +1,55 @@
+#!/usr/bin/env Rscript
+
+library(ggplot2)
+args <- commandArgs(trailingOnly=TRUE)
+input <- args[[1]]
+genen <- args[[2]]
+output1 <- args[[3]]
+
+expr <- read.table(input, sep="\t", header=FALSE)
+colnames(expr) <- c('model','expr')
+f <- expr
+#expr$sample <- as.factor(substr(expr$lsample,0,7))
+#f <- as.data.frame(sapply(levels(expr$sample), function(x) { mean(expr[expr$sample==x,'gene']) }))
+#colnames(f) <- 'expr'
+#f$model <- rownames(f)
+
+textSize <- 1
+current_theme <-
+  theme_bw() +
+  theme(
+    strip.text = element_text(size = rel(textSize)),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    axis.title = element_text(size = 12),
+    axis.text.x = element_text(size=9, angle = 90, hjust = 1, vjust=0.5),
+    #axis.text.x = element_blank(),
+    axis.text.y = element_text(angle = 0,
+                               size = 9),
+    axis.line = element_line(colour = "black"),
+    axis.ticks.x = element_blank(),
+    axis.ticks.length.y.left = unit(3,'mm'),
+    axis.line.x = element_blank(),
+    axis.line.y = element_line(),
+    legend.position = "top",
+    legend.justification = "right",
+    #legend.margin = margin(unit(0, "cm")),
+    legend.title = element_text(size = rel(textSize), face = "bold"),
+    legend.text = element_text(size = rel(textSize)),
+    legend.background = element_rect(size=0.5, linetype="solid", color="black"),
+    plot.title = element_text(
+      face = "bold",
+      size = 15,
+      hjust = 0.5
+    ),
+    panel.border = element_blank()
+  )
+f$l <- log2(f$expr+1)
+me <- median(f$l)
+save.image(paste0(output1, '.Rdata'))
+#p <- ggplot(f, aes(y=l,x=reorder(model, -l)))+geom_col(colour='black')+ylab('expr')+xlab("")+current_theme+scale_fill_manual(values=c("darkgrey","orange"))+ggtitle(genen)+geom_hline(aes(yintercept=me, linetype="1"), size=1,color="darkblue")+scale_x_discrete(expand=expansion(add=4))
+p <- ggplot(f, aes(y=l,x=reorder(model, -l)))+geom_col()+ylab('expr')+xlab("")+current_theme+ggtitle(genen)+geom_hline(aes(yintercept=me, linetype="1"), size=1,color="darkblue")+scale_x_discrete(expand=expansion(add=4))
+p <- p+scale_linetype_manual(name = "median", labels = "", values="solid") 
+
+ggsave(output1, plot=p)
+
