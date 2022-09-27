491b02763cb85a647f54143849675e92083b0951
Elena Grassi <grassi.e@gmail.com>
2022-09-27 12:32:43 +0200

    Fixed bugs in n samples for gct

--- local/src/gctize.R
+++ local/src/gctize.R
@@ -7,7 +7,7 @@ gct_f <- args[[2]]
 d <- read.table(matrix_f, sep="\t", stringsAsFactors=FALSE, row.names=NULL)
 
 d$Description <- rep("NA", nrow(d))
-d <- d[, c(1, ncol(d), seq(3, ncol(d)-1))]
+d <- d[, c(1, ncol(d), seq(2, ncol(d)-1))]
 colnames(d)[1] <- 'NAME'
 
 write.table(d, file=gct_f, append=TRUE, sep="\t", quote=FALSE, row.names=FALSE)
