4969507ef55013ebabe8fb8d0bd674d574723e7e
Elena Grassi <grassi.e@gmail.com>
2025-06-11 17:32:06 +0200

    scatter plot for RADsemilow with right theme

--- local/src/confronto_m29_semilow_EG.R
+++ local/src/confronto_m29_semilow_EG.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(ggplot2)
+library(ggrastr)
+
 m29 <- "/scratch/trcanmed/DE_RNASeq/dataset/magnifici29/type_cutoff0.05-resistant.vs.sensitive.deseq2.tsv"
 m29 <- read.table(m29, quote = "", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 m29$genes <- gsub("H_", "", rownames(m29))
@@ -7,17 +11,88 @@ rad51 <- read.table(rad51, quote = "", sep = "\t", header = TRUE, stringsAsFacto
 rad51$genes <- gsub("H_", "", rownames(rad51))
 
 merged <- merge(m29, rad51, by="genes")
+# x is m29 y is rad51 low
+ci <- cor.test(merged$log2FoldChange.x, merged$log2FoldChange.y)
+### scatter nuova suppl S12 #######################
 
-cor.test(merged$log2FoldChange.x, merged$log2FoldChange.y)
+size <- 8
 
-ggplot(data=merged, aes(x=log2FoldChange.x, y=log2FoldChange.y))+geom_point()+geom_smooth()+theme_bw(base_size=20)
+#font_add(family = "myriad", regular = snakemake@input[['myriad']])
+#showtext_auto()
 
+# Da Marti e https://www.christophenicault.com/post/understand_size_dimension_ggplot2/
+# showtext_opts(dpi = 300) 
+# since we are not changing fonts in the end cause myriad end up not being text object I'm not sure it's needed
+# showtext_auto(enable = TRUE)
+
+#textSize <- textSize * (96/72) # these conversion were needed because the default dpi for text was 96?
+# in the svg the number passed to theme was reported as size = ..px.. rather than pt (?)
+#largerSize <- largerSize * (96/72) 
+death_conversion_dpi96 = 96/72
+
+textSize <- size * death_conversion_dpi96
+largerSize <- size* death_conversion_dpi96
+
+unmute_theme <- theme(
+  text = element_text(size = textSize),#, family='Arial'),
+  axis.title = element_text(size = largerSize),
+  axis.text.x = element_text(size = textSize, color="black"),#, angle = 90, vjust = 0.5, hjust=1)
+  axis.text.y = element_text(size = textSize, color="black"),
+  plot.title = element_text(size = largerSize, hjust = 0.5),
+  legend.title = element_text(size=largerSize, hjust = 0.5),
+  legend.text = element_text(size=textSize),
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  axis.line = element_line(colour = "black", size=0.508/0.564), # origin of this ratio is honestly not known, empirical
+  axis.ticks = element_line(color = "black", size=0.508/0.564),
+  axis.ticks.length= unit(1.905*death_conversion_dpi96, "mm"),
+  panel.background = element_blank()
+)
+#axis.ticks.length= unit(1.905*death_conversion_dpi96, "mm"),
+
+
+# function that given values to be plotted on an axis will return:
+# vector of breaks, trying to guess which max will be the best one
+# this will be used as scale_y_continuous(breaks=  and as ylim(min, max) to have the - also limits-c()
+# last tick at the extremity of the axis.
+# other parameter is n. of ticks
+guess_ticks <- function(values, nticks=5, fixed_max=NULL, fixed_min=0) {
+  vmax <- max(values)
+  if (is.null(fixed_max)) { 
+    round_max <- ceiling(vmax)
+  } else {
+    round_max <- fixed_max
+  }
+  my_breaks <- seq(fixed_min, round_max, length.out=nticks)
+  return(my_breaks)
+}
+
+ggplot(data=merged, aes(x=log2FoldChange.x, y=log2FoldChange.y))+geom_point()+geom_smooth()+theme_bw(base_size=20)+
+  xlab('log2FoldChange_reference_subset')+ylab('log2FoldChange_RADsemilow')
+
+
+y_breaks <- guess_ticks(merged$log2FoldChange.y, fixed_min=-2, fixed_max=3)
+x_breaks <- guess_ticks(merged$log2FoldChange.x, fixed_min=-2, fixed_max=3)
+
+p <- ggplot(merged, aes(x=log2FoldChange.x, y=log2FoldChange.y)) + 
+  rasterise(geom_point(size = 1), dpi=300)+
+  geom_smooth(method=lm, se=FALSE)+ # ratio is ... 1 becomes 0.939
+  unmute_theme+theme(legend.position="none") + xlab('log2FoldChange_reference_subset')+ylab('log2FoldChange_RADsemilow')+
+  scale_y_continuous(breaks=y_breaks, limits=c(min(y_breaks),max(y_breaks)), expand=c(0,0))+
+  scale_x_continuous(breaks=x_breaks, limits=c(min(x_breaks),max(x_breaks)), expand=c(0,0))
+
+#ggsave(p, file="/scratch/trcanmed/DE_RNASeq/dataset/m29_new_chemio_groups/scatter_m29_grigi.pdf", width=89*(death_conversion_dpi96), height=89*(death_conversion_dpi96), units="mm")
+ggsave(p, file="/scratch/trcanmed/DE_RNASeq/dataset/rad51_res.vs.sens/scatter_m29_radsemilow.pdf", width=89*(death_conversion_dpi96), height=89*(death_conversion_dpi96), units="mm")
+
+######################
 h_m29_f <- "/scratch/trcanmed/DE_RNASeq/dataset/magnifici29/GSEA_results_H_type_cutoff0.05-resistant.vs.sensitive.tsv"
 h_m29 <- read.table(h_m29_f, quote = "", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
 h_r_f <- "/scratch/trcanmed/DE_RNASeq/dataset/rad51_res.vs.sens/GSEA_results_H_type_cutoff0.05-resistant.vs.sensitive.tsv"
 h_r <- read.table(h_r_f, quote = "", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
+
+merged_h <- merge(h_r, h_m29, by="ID")
+cor.test(merged_h$enrichmentScore.x, merged_h$enrichmentScore.y)
 # h_w3 <- h_w3 %>% filter(p.adjust < 0.05)
 # 
 # h_m29 <- h_m29 %>% filter(p.adjust < 0.05)
@@ -57,6 +132,10 @@ c2_r <- read.table(c2_r_f, quote = "", sep = "\t", header = TRUE, stringsAsFacto
 c2_m29_f <- "/scratch/trcanmed/DE_RNASeq/dataset/magnifici29/GSEA_results_C2_type_cutoff0.05-resistant.vs.sensitive.tsv"
 c2_m29 <- read.table(c2_m29_f, quote = "", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
 
+
+merged_h <- merge(c2_r, c2_m29, by="ID")
+cor.test(merged_h$enrichmentScore.x, merged_h$enrichmentScore.y)
+
 # c2_w3 <- c2_w3 %>% filter(p.adjust < 0.05)
 # 
 # c2_m29 <- c2_m29 %>% filter(p.adjust < 0.05)
@@ -94,6 +173,9 @@ c6_r <- read.table(c6_r_f, quote = "", sep = "\t", header = TRUE, stringsAsFacto
 
 c6_m29_f <- "/scratch/trcanmed/DE_RNASeq/dataset/magnifici29/GSEA_results_C6_type_cutoff0.05-resistant.vs.sensitive.tsv"
 c6_m29 <- read.table(c6_m29_f, quote = "", sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+
+merged_h <- merge(c6_r, c6_m29, by="ID")
+cor.test(merged_h$enrichmentScore.x, merged_h$enrichmentScore.y)
 
 #c6_w3 <- c6_w3 %>% filter(p.adjust < 0.05)
 
